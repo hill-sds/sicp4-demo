5c5181633797ce4be11bad91e46310b865930634
shen-dsh <1114325898@qq.com>
2021-02-25 16:37:51 +0800

    集成Swagger

--- demo/src/main/java/com/springBoot/demo/mybatisDemo/controller/UserController.java
+++ demo/src/main/java/com/springBoot/demo/mybatisDemo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springBoot.demo.mybatisDemo.controller;
 import com.springBoot.demo.mybatisDemo.common.dto.ResultDTO;
 import com.springBoot.demo.mybatisDemo.entity.User;
 import com.springBoot.demo.mybatisDemo.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("swaggerDemoController相关的api")
 public class UserController {
 
     @Autowired
@@ -20,7 +22,7 @@ public class UserController {
      * @param id
      * @return
      */
-    @RequestMapping("getUser/{id}")
+    @PostMapping("getUser/{id}")
     public ResultDTO GetUser(@PathVariable int id){
         User user = service.sel(id);
         return new ResultDTO("0", user.toString());
